d9eb59deb79c159d4c901c77cd8abf465c750d04
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-08-01 23:06:58 +0600

    Just create a frame

---
+++ MyFrame.java
@@ -0,0 +1,1 @@
+package GUIBasic13;import javax.swing.*;public class MyFrame {    public static void main(String[] args) {        JFrame frame = new JFrame("My Frame");        frame.setSize(400, 300);        frame.setLocationRelativeTo(null);        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        frame.setVisible(true);    }}
